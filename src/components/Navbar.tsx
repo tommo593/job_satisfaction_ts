7e8f192849d577578a51227311ad62d3ab009ca3
tommo593 <tomdevwork@gmail.com>
2024-04-15 19:25:59 +0100

    Styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 const Navbar = () => {
   return (
-    <div>
-      <h1 className="text-center">Employee Satisfaction Survey</h1>
+    <div className="flex flex-row justify-between">
+      <h1 className="text-center m-auto">Employee Satisfaction Survey</h1>
       <div>
         <button
           type="submit"
-          title="Login"
-          className="font-bold transition-all hover:bg-purple-900 active:bg-purple-600 rounded p-2 justify-end"
-        ></button>
+          className="font-bold transition-all hover:bg-purple-900 active:bg-purple-600 rounded p-2"
+        >
+          Login
+        </button>
       </div>
     </div>
   );
