a68bf31988bad7975a08bafad8dbaf031cd92cfd
tommo593 <tomdevwork@gmail.com>
2024-04-26 02:09:11 +0100

    Changes to colour scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./App.css";
 import "./index.css";
 import Slider from "./components/Slider";
 import Header from "./components/Header";
@@ -39,33 +38,33 @@ const App: React.FC = () => {
           <Navbar />
         </div>
       </div>
-      <div className="px-4">
+      <div className="px-4 m-auto">
         <div className="py-8">
           <Header />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>
             How likely is it that you'll get promoted within the next 2 years?
           </p>
           <Slider question="q1" onSliderChange={handleSliderChange} />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>How much do you enjoy your current role?</p>
           <Slider question="q2" onSliderChange={handleSliderChange} />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>How satisfied are you with your current pay?</p>
           <Slider question="q3" onSliderChange={handleSliderChange} />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>How would you rate your development since starting with us?</p>
           <Slider question="q4" onSliderChange={handleSliderChange} />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>How satisfied are you with the level of support you receive?</p>
           <Slider question="q5" onSliderChange={handleSliderChange} />
         </div>
-        <div className="py-8 mb-4 rounded-lg bg-purple-400">
+        <div className="py-8 mb-4 rounded-lg bg-slider_bg">
           <p>How satisfied are you with your contributions so far?</p>
           <Slider question="q6" onSliderChange={handleSliderChange} />
         </div>
--- src/components/Results.tsx
+++
@@ -1,7 +0,0 @@
-const Results = () => {
-  <div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
-  </div>;
-};
-
-export default Results;
