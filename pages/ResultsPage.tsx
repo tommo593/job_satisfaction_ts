8920ba9fda66b4035991480772bbd8d2e2426f6e
tommo593 <tomdevwork@gmail.com>
2024-05-04 19:25:44 +0100

    Stuff

--- pages/ResultsPage.tsx
+++ pages/ResultsPage.tsx
@@ -8,14 +8,16 @@ interface Props {
 
 const ResultsPage: React.FC<Props> = ({ totalScore }) => {
   const scoreResults = () => {
+    let result;
     if (totalScore < 21) {
-      return <p>You earned a low score</p>;
+      result = <p>You earned a low score</p>;
     } else if (totalScore > 20 && totalScore < 40) {
-      return <p>You earned a mid score</p>;
+      result = <p>You earned a mid score</p>;
     } else {
-      return <p>You earned a high score</p>;
+      result = <p>You earned a high score</p>;
     }
-    console.log({ scoreResults });
+    console.log({ result }); // Move console.log here
+    return result; // Return result here
   };
 
   return (
