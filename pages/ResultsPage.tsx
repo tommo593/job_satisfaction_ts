3ac3ca5aa7379fb69c1cf2ae8b330b296e18322f
tommo593 <tomdevwork@gmail.com>
2024-05-04 15:33:24 +0100

    if else statement fixed

--- pages/ResultsPage.tsx
+++ pages/ResultsPage.tsx
@@ -1,37 +1,26 @@
+import React from "react";
 import Footer from "../src/components/Footer";
+import Navbar from "../src/components/Navbar";
 
-const ResultsPage = () => {
-  if (totalScore <= 20) {
-    return (
-      <p>
-        You scored {totalScore} out of a potential 60 points. We're sorry we're
-        not meeting the standards as you expected. Please contact your manager
-        and arrange a 1:1 as soon as possible to find out what we can do to
-        improve.
-      </p>
-    );
-  } else if (totalScore <= 40) {
-    return (
-      <p>
-        You scored {totalScore} out of a potential 60 points. This could
-        indicate that there is some areas for us both to improve. Please contact
-        your manager and arrange a 1:1 to find out where you can improve!
-      </p>
-    );
-  } else {
-    if (totalScore > 40) {
-      return (
-        <p>
-          You scored {totalScore} out of a potential 60 points. We're happy that
-          you're happy, keep up the great work and we're sure you'll go far in
-          this company!
-        </p>
-      );
+interface Props {
+  totalScore: number;
+}
+
+const ResultsPage: React.FC<Props> = ({ totalScore }) => {
+  const scoreResults = () => {
+    if (totalScore < 21) {
+      return <p>You earned a low score</p>;
+    } else if (totalScore > 20 && totalScore < 40) {
+      return <p>You earned a mid score</p>;
+    } else {
+      return <p>You earned a high score</p>;
     }
-  }
+  };
 
   return (
     <div>
+      <Navbar />
+      {scoreResults()}
       <Footer />
     </div>
   );
